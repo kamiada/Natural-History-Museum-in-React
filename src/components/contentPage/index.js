9a0d137227af4ce1e64197f7a93a3f6906e329ba
kamiada <kamiada528@gmail.com>
2021-05-18 07:29:06 +0100

    pages pages pages

--- src/components/contentPage/index.js
+++ src/components/contentPage/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Layout from '../Layout/layout';
+import ContentPageBackground from '../../images/claudio-testa-iqeG5xA96M4-unsplash.jpg';
+import '../../global_style.scss';
 
 const Content = () => {
     return (
         <>
         <Layout>
-            <div>
+            <img src={ContentPageBackground} alt="museum" ></img>
+            <div className="content_wrapper">
                 Content page
             </div>
         </Layout>
--- src/components/homePage/index.js
+++ src/components/homePage/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Layout from '../Layout/layout';
 import BackgroundImage from '../../images/kevin-mueller-1gUES9J7Ph8-unsplash.jpg'
+import '../../global_style.scss';
+import './home_style.scss';
 
 const Home = () => {
     return (
         <>
         <Layout>
-            <div className="home_wrapper">
             <img src={BackgroundImage} alt="museum" ></img>
-                Home page
+            <div className="home_wrapper">
+               <h1>
+               WELCOME TO NATURAL HISTORY MUSEUM
+               </h1> 
             </div>
         </Layout>
         </>
