5977587b4532770e1bca02ba797f34656d38d31f
kamiada <kamiada528@gmail.com>
2021-05-20 18:46:32 +0100

    main page a bit hard coded, but ey looks nice on my machine

--- src/components/homePage/index.js
+++ src/components/homePage/index.js
@@ -1,32 +1,34 @@
-import React from 'react';
-import Layout from '../Layout/layout';
-import BackgroundImage from '../../images/kevin-mueller-1gUES9J7Ph8-unsplash.jpg'
-import '../../global_style.scss';
-import './home_style.scss';
+import React from "react";
+import Layout from "../Layout/layout";
+import BackgroundImage from "../../images/kevin-mueller-1gUES9J7Ph8-unsplash.jpg";
+import "../../global_style.scss";
+import "./home_style.scss";
 
 const Home = () => {
-    return (
-        <>
-        <Layout>
-            <img src={BackgroundImage} alt="museum" ></img>
-            <div className="home_title_wrapper">
-
-            <div className="menu_background">
-            <img src={BackgroundImage} alt="museum" ></img>
-            <h1>
-               WELCOME TO NATURAL HISTORY MUSEUM
-               </h1> 
-               <button className="home_button">
-               <p>
-               BOOK YOUR FREE TICKETS IN ADVANCE
-               </p>
-               </button>
-            </div>
-
-            </div>
-        </Layout>
-        </>
-    )
-}
+  return (
+    <>
+      <Layout>
+        <img src={BackgroundImage} alt="museum"></img>
+        <div className="home_title_wrapper">
+        <div className="title_holder">
+            <h1>WELCOME TO NATURAL HISTORY MUSEUM</h1>
+            <button className="home_button">
+              <p>BOOK YOUR FREE TICKETS IN ADVANCE</p>
+            </button>
+          </div>
+          <div className="home_links">
+          <a>VISIT</a>
+          <a>DISCOVER</a>
+          <a>TRY AT HOME</a>
+          <a>JOIN AND SUPPORT</a>
+          <a>SHOP</a>
+          <a>SCHOOLS</a>
+          <a>OUR SCIENCE</a>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
 
 export default Home;
