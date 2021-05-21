7cee82208c326c2448da06fedc522178c5d25d88
kamiada <kamiada528@gmail.com>
2021-05-21 12:37:01 +0100

    time for content

--- src/components/contentPage/index.js
+++ src/components/contentPage/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../Layout/layout';
 import ContentPageBackground from '../../images/claudio-testa-iqeG5xA96M4-unsplash.jpg';
 import '../../global_style.scss';
+import './content_styles.scss';
 
 const Content = () => {
     return (
