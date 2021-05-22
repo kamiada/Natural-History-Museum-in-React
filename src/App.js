91bcc2bc7eb33d1769806a80c9ea24dcbe0dd58c
kamiada <kamiada528@gmail.com>
2021-05-22 21:00:59 +0100

    more styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import React, { Fragment } from 'react';
-import Home from '../src/components/homePage/index';
-import Content from './components/contentPage';
+import "./App.css";
+import React, { Fragment } from "react";
+import Home from "../src/components/homePage/index";
+import Content from "./components/contentPage";
 
 function App() {
   return (
     <Fragment>
-    <Home />
-    <Content />
+      <Home />
+      <Content />
     </Fragment>
   );
 }
