680fe87e5008e5826eaa26a17393d43e0ff20180
kamiada <kamiada528@gmail.com>
2021-05-22 20:36:19 +0100

    set up for content

--- src/components/contentPage/index.js
+++ src/components/contentPage/index.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import Layout from '../Layout/layout';
-import ContentPageBackground from '../../images/claudio-testa-iqeG5xA96M4-unsplash.jpg';
-import '../../global_style.scss';
-import './content_styles.scss';
+import React from "react";
+import Layout from "../Layout/layout";
+import ContentPageBackground from "../../images/claudio-testa-iqeG5xA96M4-unsplash.jpg";
+import "../../global_style.scss";
+import "./content_styles.scss";
 
 const Content = () => {
-    return (
-        <>
-        <Layout>
-            <div className="content_wrapper">
-                Content page
-            </div>
-        </Layout>
-        </>
-    )
-}
+  return (
+    <>
+      <Layout>
+        <div className="content_wrapper">
+        <img src={ContentPageBackground} alt="museum"></img>
+          <div className="panel_holder">
+            <div>panel 1</div>
+            <div className="long_panel">panel 2</div>
+            <div>panel 3</div>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
 
 export default Content;
