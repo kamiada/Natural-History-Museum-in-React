dc6672997f39e55888ff68892d9a9ad2be3c8386
kamiada <kamiada528@gmail.com>
2021-05-23 10:20:04 +0100

    for now

--- src/components/contentPage/index.js
+++ src/components/contentPage/index.js
@@ -5,84 +5,25 @@ import ContentPageBackground from "../../images/claudio-testa-iqeG5xA96M4-unspla
 import "../../global_style.scss";
 import "./content_styles.scss";
 import ourScience from "../../images/paul-minami-_AH4fdMyYIg-unsplash.jpg";
-import Tile from './Tile';
-
-// const Content = () => {
-//   return (
-//     <>
-//       <Layout>
-//         <div className="content_wrapper">
-//           {/* <img src={ContentPageBackground} alt="museum"></img> */}
-//           <div className="panel_holder">
-//             <div className="square_panel_holder">
-//               <div className="square">
-//                 <div className="square__image">
-//                   {/* <img src={ourScience} alt="image of museum"></img> */}
-//                 </div>
-//                 <div className="square__text">Our science</div>
-//               </div>
-//               <div className="square">
-//                 <div className="square__image">
-//                   {/* <img src={ourScience} alt="image of museum"></img> */}
-//                 </div>
-//                 <div className="square__text">Become our member</div>
-//               </div>
-//               <div className="square">
-//                 <div className="square__image">
-//                   {/* <img src={ourScience} alt="image of museum"></img> */}
-//                 </div>
-//                 <div className="square__text">Try at home</div>
-//               </div>
-//             </div>
-//             <div className="long_panel">
-//               <div className="title">
-//                 OUR BROKEN PLANET: How we got here and ways to fix it
-//               </div>
-//             </div>
-//             <div className="square_panel_holder">
-//               <div className="square">
-//                 <div className="square__image">
-//                   {/* <img src={ourScience} alt="image of museum"></img> */}
-//                 </div>
-//                 <div className="square__text">British wildlife</div>
-//               </div>
-
-//               <div className="square">
-//                 <div className="square__image">
-//                   {/* <img src={ourScience} alt="image of museum"></img> */}
-//                 </div>
-//                 <div className="square__text">Anthropocene</div>
-//               </div>
-
-//               <div className="square">
-//                 <div className="square__image">
-//                   {/* <img src={ourScience} alt="image of museum"></img> */}
-//                 </div>
-//                 <div className="square__text">Science News</div>
-//               </div>
-
-//               <div className="square">
-//                 <div className="square__image">
-//                   {/* <img src={ourScience} alt="image of museum"></img> */}
-//                 </div>
-//                 <div className="square__text">What on Earth?</div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </Layout>
-//     </>
-//   );
-// };
+import Tile from "./Tile";
 
 const Content = () => {
   return (
     <>
       <Layout>
         <div className="content_wrapper">
-        <Tile image={ourScience} alt_text="museum" text="Our science"/>
-        <Tile image={ourScience} alt_text="museum" text="Our science"/>
-        <Tile image={ourScience} alt_text="museum" text="Our science"/>
+          <Tile image={ourScience} alt_text="museum" text="Our science" />
+          <Tile image={ourScience} alt_text="museum" text="Become our member" />
+          <Tile image={ourScience} alt_text="museum" text="Try at home" />
+          {/* <div className="long_panel">
+            <div className="title">
+              OUR BROKEN PLANET: How we got here and ways to fix it
+            </div>
+          </div> */}
+          <Tile image={ourScience} alt_text="museum" text="British wildlife" />
+          <Tile image={ourScience} alt_text="museum" text="Anthropocene" />
+          <Tile image={ourScience} alt_text="museum" text="Science News" />
+          <Tile image={ourScience} alt_text="museum" text="What on Earth?" />
         </div>
       </Layout>
     </>
