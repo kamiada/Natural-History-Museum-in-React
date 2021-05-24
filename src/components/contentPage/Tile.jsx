3234c12a7378b4c00e2c7b0349fb1ad445d63884
kamiada <kamiada528@gmail.com>
2021-05-24 20:59:55 +0100

    more stuff

--- src/components/contentPage/Tile.jsx
+++ src/components/contentPage/Tile.jsx
@@ -4,12 +4,10 @@ import "./content_styles.scss";
 class Tile extends Component {
     render() {
         return (
-            <div className="square">
             <div className="square__image">
               <img src={this.props.image} alt={this.props.alt_text}></img>
               <div className="square__text">{this.props.text}</div>
             </div>
-          </div>
         )
     }
 }
--- src/components/contentPage/index.js
+++ src/components/contentPage/index.js
@@ -6,6 +6,12 @@ import "../../global_style.scss";
 import "./content_styles.scss";
 import ourScience from "../../images/paul-minami-_AH4fdMyYIg-unsplash.jpg";
 import Tile from "./Tile";
+import Burger from "../../images/armando-ascorve-morales-ypZI_CA91M0-unsplash.jpg";
+import Skull from '../../images/mathias-p-r-reding-JvjPYMUr3Pk-unsplash.jpg';
+import Hall from '../../images/claudio-testa-iqeG5xA96M4-unsplash.jpg';
+import BritishWildlife from '../../images/pauline-bernfeld-A-Xd4AADhcU-unsplash.jpg';
+import Whale from '../../images/fabio-hanashiro-ur4pyEnPdA0-unsplash.jpg';
+import Spider from '../../images/hello-katuvi-0VDXcJuQATg-unsplash.jpg';
 
 const Content = () => {
   return (
@@ -13,17 +19,17 @@ const Content = () => {
       <Layout>
         <div className="content_wrapper">
           <Tile image={ourScience} alt_text="museum" text="Our science" />
-          <Tile image={ourScience} alt_text="museum" text="Become our member" />
-          <Tile image={ourScience} alt_text="museum" text="Try at home" />
+          <Tile image={Skull} alt_text="museum" text="Become our member" />
+          <Tile image={Hall} alt_text="museum" text="Try at home" />
           {/* <div className="long_panel">
             <div className="title">
               OUR BROKEN PLANET: How we got here and ways to fix it
             </div>
           </div> */}
-          <Tile image={ourScience} alt_text="museum" text="British wildlife" />
-          <Tile image={ourScience} alt_text="museum" text="Anthropocene" />
-          <Tile image={ourScience} alt_text="museum" text="Science News" />
-          <Tile image={ourScience} alt_text="museum" text="What on Earth?" />
+          <Tile image={BritishWildlife} alt_text="museum" text="British wildlife" />
+          <Tile image={Burger} alt_text="museum" text="Anthropocene" />
+          <Tile image={Whale} alt_text="museum" text="Science News" />
+          <Tile image={Spider} alt_text="museum" text="What on Earth?" />
         </div>
       </Layout>
     </>
