c9b9ab9c2ec3811f6376c6b0f22b0d1316664214
kamiada <kamiada528@gmail.com>
2021-06-14 19:47:27 +0100

    link page

---
+++ src/components/linkPage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../Layout/layout";
+import './link_page_style.scss';
+
+const linkPage = () => {
+<>
+<Layout>
+    
+</Layout>
+</>
+}
+export default linkPage;
